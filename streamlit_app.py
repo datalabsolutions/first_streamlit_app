9418ffea32ff257847bcf5b52acb6783a761e4ab
datalabsolutions <116706129+datalabsolutions@users.noreply.github.com>
2022-10-29 08:00:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
   
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 and Bluberyy Outmeal")
+streamlit.text("Kale, Spinich and Rocket Smoothie")
+streamlit.text("Hard-Boiled, Free-Range Egg")
